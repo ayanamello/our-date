d4462b3fe8160c658ffc4b59be4ba57abe223490
Ayana <ayanamello27@gmail.com>
2022-06-07 23:57:00 -0300

    feat: add dates bd

---
+++ src/utils/bd.js
@@ -0,0 +1,257 @@
+export const dates = [
+    {
+        id: 1,
+        name: "Ir ao Racletto",
+        local: "Horto Florestal"
+    },
+    {
+        id: 2,
+        name: "Ir ao Mr. Côco",
+        local: "Food Park Salvador"
+    },
+    {
+        id: 3,
+        name: "Ir ao Baía Sunset Bar",
+        local: "Largo dos Aflitos"
+    },
+    {
+        id: 4,
+        name: "Ir ao Bombar",
+        local: "Rio Vermelho"
+    },
+    {
+        id: 5,
+        name: "Ir à Amsterdan",
+        local: "Rio Vermelho"
+    },
+    {
+        id: 6,
+        name: "Fazer, juntas, uma refeição completa com entrada, principal e sobremesa",
+        local: ""
+    },
+    {
+        id: 7,
+        name: "Ir ao Groove Burger",
+        local: "Cabula"
+    },
+    {
+        id: 8,
+        name: "Fazer um piquenique",
+        local: ""
+    },
+    {
+        id: 9,
+        name: "Ir a um restaurante mexicano",
+        local: ""
+    },
+    {
+        id: 10,
+        name: "Ir ao Museu da Música",
+        local: "Pelourinho"
+    },
+    {
+        id: 11,
+        name: "Ir ao RoofTop",
+        local: "Liberdade"
+    },
+    {
+        id: 12,
+        name: "Ir à Marttini Pizza",
+        local: "Costa Azul"
+    },
+    {
+        id: 13,
+        name: "Ir ao shopping e uma comprar um presente para outra",
+        local: ""
+    },
+    {
+        id: 14,
+        name: "Ir em um karaokê",
+        local: ""
+    },
+    {
+        id: 15,
+        name: "Jantar em um lugar chique",
+        local: ""
+    },
+    {
+        id: 16,
+        name: "Ir à Villa San Luigi",
+        local: "Pituba"
+    },
+    {
+        id: 17,
+        name: "Ir ao boliche",
+        local: "Shopping Bela Vista"
+    },
+    {
+        id: 18,
+        name: "Passear no Pelourinho",
+        local: ""
+    },
+    {
+        id: 19,
+        name: "Ir ao Mercado Modelo e almoçar lá",
+        local: "Cidade Baixa"
+    },
+    {
+        id: 20,
+        name: "Ir ao MAM",
+        local: "Comércio"
+    },
+    {
+        id: 21,
+        name: "Ir ao Mahi Mahi",
+        local: "Vitória"
+    },
+    {
+        id: 22,
+        name: "Ir ao Hu Pastel",
+        local: "Barra"
+    },
+    {
+        id: 23,
+        name: "Ir à Ilha dos Frades",
+        local: ""
+    },
+    {
+        id: 24,
+        name: "Ir ao Museu do Mar",
+        local: "Largo Santo Antônio Além do Carmo"
+    },
+    {
+        id: 25,
+        name: "Ir à Red Burger",
+        local: "Barra"
+    },
+    {
+        id: 26,
+        name: "Ir ao restaurante Barravento", 
+        local: "Barra"
+    },
+    {
+        id: 27,
+        name: "Ir ao Larri Bistrô",
+        local: "Barra"
+    },
+    {
+        id: 28,
+        name: "Ir à La Empa",
+        local: "Pituba"
+    },
+    {
+        id: 29,
+        name: "Fazer a trilha ecológica da UCSAL",
+        local: ""
+    },
+    {
+        id: 30,
+        name: "Ir à Jumbo Slice",
+        local: "Rio Vermelho"
+    },
+    {
+        id: 31,
+        name: "Ir ao rodízio de drinks da Oxe Drinks",
+        local: "Pituba"
+    },
+    {
+        id: 32,
+        name: "Ir ao Palles",
+        local: "Costa Azul"
+    },
+    {
+        id: 33,
+        name: "Ir assistir a uma peça de teatro",
+        local: ""
+    },
+    {
+        id: 34,
+        name: "Ir à Brutus Burger",
+        local: "Pituba ou Salvador Shopping"
+    },
+    {
+        id: 35,
+        name: "Ir ao São Jogue",
+        local: "Shopping Bela Vista"
+    },
+    {
+        id: 36,
+        name: "Ir ao circo",
+        local: ""
+    },
+    {
+        id: 37,
+        name: "Ir ao Quiosque do Orgânico",
+        local: "Shopping Salvador"
+    },
+    {
+        id: 38,
+        name: "Ir ao Kiputaria",
+        local: "Barra"
+    },
+    {
+        id: 39,
+        name: "Ir ao Ta Rebocado",
+        local: "Barra"
+    },
+    {
+        id: 40,
+        name: "Ir à Scappi Trattoria",
+        local: "Salvador Norte Shopping"
+    },
+    {
+        id: 41,
+        name: "Ir ao Sniper",
+        local: "Salvador Norte Shopping"
+    },
+    {
+        id: 42,
+        name: "Ir ao Wikipoke",
+        local: "Shopping Paralela"
+    },
+    {
+        id: 43,
+        name: "Ir ao Poró Restaurante Bar",
+        local: ""
+    },
+    {
+        id: 44,
+        name: "Ir ao Happy Hour Pizzaria",
+        local: "Rio Varmelho"
+    },
+    {
+        id: 45,
+        name: "Ir ao Marreco",
+        local: "Rio Varmelho"
+    },
+    {
+        id: 46,
+        name: "Ir à Cosi Pizzaria",
+        local: "Rio Varmelho"
+    },
+    {
+        id: 47,
+        name: "Ir à Vila Brandão",
+        local: "Rio Varmelho"
+    },
+    {
+        id: 48,
+        name: "Ir a uma aula de dança",
+        local: ""
+    },
+    {
+        id: 49,
+        name: "Ir a um show de comédia",
+        local: ""
+    },
+    {
+        id: 50,
+        name: "Fazer um jantar à luz de velas",
+        local: ""
+    },
+    {
+        id: 51,
+        name: "Fazer uma sobremesa juntas",
+        local: ""
+    },
+]
